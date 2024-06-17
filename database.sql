cc688a0015c97f0869f6a840f02da34080cda779
Alina <alicirelli@gmail.com>
2024-06-17 16:08:57 +0200

    Ajout des requetes d'insertion employes

--- database.sql
+++ database.sql
@@ -3,3 +3,7 @@ CREATE TABLE employees (
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL
 );
+
+-- Requêtes d'insertion pour ajouter deux employés de test
+INSERT INTO employees (first_name, last_name) VALUES ('Micaela', 'Gonzalez');
+INSERT INTO employees (first_name, last_name) VALUES ('Clemence', 'Dye');
